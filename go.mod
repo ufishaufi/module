4dd9c8af90494db22570e47ef71fef973e1e0f4e
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2024-01-21 08:29:31 +0800

    Major Upgrade for update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ufishaufi/module
+module github.com/ufishaufi/module/v2
 
 go 1.20
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package module
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
